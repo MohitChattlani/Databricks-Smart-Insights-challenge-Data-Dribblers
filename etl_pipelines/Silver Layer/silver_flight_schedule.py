5c7be2c21974b3db0c08c4507f2f7dfba9542882
MohitChattlani <chattlanimohit@gmail.com>
2025-05-20 12:49:32 +0530

    Handled null values in scheduledDepartureTime and scheduledArrivalTime

--- etl_pipelines/Silver Layer/silver_flight_schedule.py
+++ etl_pipelines/Silver Layer/silver_flight_schedule.py
@@ -3,14 +3,29 @@ bronze_layer_flight_schedule_df=spark.table("bronze_lyr.flight_schedule")
 
 # COMMAND ----------
 
-display(bronze_layer_flight_schedule_df)
+from pyspark.sql.functions import col, trim, length
+
+bronze_layer_flight_schedule_filtered_df = bronze_layer_flight_schedule_df.where(
+    (col("scheduledDepartureTime").isNotNull()) &
+    (length(trim(col("scheduledDepartureTime"))) > 0) &
+    (col("scheduledArrivalTime").isNotNull())  &
+    (length(trim(col("scheduledArrivalTime")))  > 0)
+)
+
+# COMMAND ----------
+
+bronze_layer_flight_schedule_filtered_df.count()
+
+# COMMAND ----------
+
+display(bronze_layer_flight_schedule_filtered_df)
 
 # COMMAND ----------
 
 #changing date columns
 from pyspark.sql.functions import col,to_date
 
-silver_layer_flight_schedule_df=bronze_layer_flight_schedule_df\
+silver_layer_flight_schedule_df=bronze_layer_flight_schedule_filtered_df\
     .withColumn("validFrom",to_date(col("validFrom"),"dd-MM-yyyy"))\
     .withColumn("validTo",to_date(col("validTo"),"dd-MM-yyyy"))
 
